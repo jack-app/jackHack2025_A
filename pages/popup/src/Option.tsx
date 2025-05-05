44997bbe1b300807186c606d688ccf93e5a932ba
Shun <shu.n.ito@icloud.com>
2025-05-05 18:23:11 +0900

    ボタンのデザインを変更

--- pages/popup/src/Option.tsx
+++ pages/popup/src/Option.tsx
@@ -1,71 +1,44 @@
 import '@src/Option.css';
 import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
+import { useState } from 'react';
+const Option = () => {
+  const [isPushed, setIsPushed] = useState<boolean>(false);
+  return (
+    <div className="buttons">
+      <button
+        id="push1"
+        onClick={() => setIsPushed(!isPushed)}
+        className="yuruyuru"
+        type="button"
+        name="name"
+        value="value">
+        {/* // isPushedの真偽値で表示するかどうかを切り替える 
+     {isPushed && (<img
+       src="c:/Users/shuni/Downloads/ChatGPT_Image_2025年5月5日_11_41_10-removebg-preview.png"
+       width="70"
+       height="70"
+       alt="ボタン画像"
+      />)} */}
+      </button>
 
-const Option = () => (
-  <>
-    <div>
-      <button
-        type="button"
-        name="name"
-        value="value"
-        style={{
-          background: '#ccff00',
-          color: '#ff9911',
-          boxShadow: '0 5px 0 rgb(153, 204, 102)',
-          borderRadius: '0.25em',
-          padding: '0.05em 0.05em 0.05em',
-          lineHeight: 1,
-          display: 'inline-block',
-          cursor: 'pointer',
-        }}>
+      <button className="hutuu" type="button" name="name" value="value">
         <img
-          src="c:\Users\shuni\Downloads\ChatGPT_Image_2025年5月5日_11_41_10-removebg-preview.png"
+          src="c:/Users/shuni/Downloads/ChatGPT_Image_2025年5月5日_11_41_10-removebg-preview.png"
           width="70"
           height="70"
+          alt="ボタン画像"
         />
       </button>
-      <button
-        type="button"
-        name="name"
-        value="value"
-        style={{
-          background: '#ff9911',
-          color: '#ff9911',
-          boxShadow: '0 5px 0rgb(216, 151, 10)',
-          borderRadius: '0.25em',
-          padding: '0.05em 0.05em 0.05em',
-          lineHeight: 1,
-          display: 'inline-block',
-          cursor: 'pointer',
-        }}>
+      <button className="gitigiti" type="button" name="name" value="value">
         <img
-          src="c:\Users\shuni\Downloads\ChatGPT_Image_2025年5月5日_11_41_10-removebg-preview.png"
+          src="c:/Users/shuni/Downloads/ChatGPT_Image_2025年5月5日_11_41_10-removebg-preview.png"
           width="70"
           height="70"
-        />
-      </button>
-      <button
-        type="button"
-        name="name"
-        value="value"
-        style={{
-          background: '#ff3300',
-          color: '#ff9911',
-          boxShadow: '0 5px 0rgb(216, 151, 10)',
-          borderRadius: '0.25em',
-          padding: '0.05em 0.05em 0.05em',
-          lineHeight: 1,
-          display: 'inline-block',
-          cursor: 'pointer',
-        }}>
-        <img
-          src="c:\Users\shuni\Downloads\ChatGPT_Image_2025年5月5日_11_41_10-removebg-preview.png"
-          width="70"
-          height="70"
+          alt="ボタン画像"
         />
       </button>
     </div>
-  </>
-);
+  );
+};
 
 export default withErrorBoundary(withSuspense(Option, <div>Loading...</div>), <div>Error Occurred</div>);
